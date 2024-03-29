de0c2dcc46ce6b4310f421a505404d2b92686133
gmsarti <gmsarti@gmail.com>
2024-03-29 19:32:41 +0000

    Mudança na descrição da página em Construção

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
 function Home() {
   return (
-    <h1>Máquina Mundos: Desvendando Cidades Fantásticas</h1>,
-    <p>Máquina Mundos é um projeto do coletivo Máquina Tudo para abrigar uma miríade de cidades fantásticas tecidas por automatos invisíveis. Aqui, você embarca em uma jornada inesquecível por paisagens oníricas, culturas exóticas e histórias envolventes, todas geradas por algoritmos criativos.</p>,
+    <h1>Máquina Mundos: Desvendando Cidades Fantásticas</h1>
+    <p>Máquina Mundos é um projeto do coletivo Máquina Tudo para abrigar uma miríade de cidades fantásticas tecidas por automatos invisíveis. Aqui, você embarca em uma jornada inesquecível por paisagens oníricas, culturas exóticas e histórias envolventes, todas geradas por algoritmos criativos.</p>
 
-    <h2>Explore</h2>,
+    <h2>Explore</h2>
     <ul>
       <li><a href="/cidades">Cidades:</a> Descubra uma variedade de cidades vibrantes, desde metrópoles futuristas até aldeias encantadas, cada uma com seus próprios costumes, habitantes e segredos.</li>
       <li><a href="/historias">Histórias:</a> Mergulhe em narrativas emocionantes que se desenrolam nas cidades, desvendando mistérios, enfrentando desafios e tecendo laços com personagens singulares.</li>
       <li><a href="/criatividade">Criatividade:</a> Explore a mente da inteligência artificial e como ela é capaz de gerar mundos fantásticos com detalhes impressionantes e originalidade sem limites.</li>
-    </ul>,
+    </ul>
 
-    <h2>Junte-se a nós</h2>,
+    <h2>Junte-se a nós</h2>
 
     <ul>
       <li><a href="/compartilhe">Compartilhe:</a> Envie suas próprias cidades fantásticas criadas com inteligência artificial e contribua para a expansão do Máquina Mundos.</li>
